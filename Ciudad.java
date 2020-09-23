536fadca1ef23ea9c376a1af81a2c821672bf403
MGa2018 <mgsalina2016@gmail.com>
2020-09-23 16:30:00 -0300

    commit1

---
+++ Ciudad.java
@@ -0,0 +1,10 @@
+package Parte1;
+
+public class Ciudad {
+	private String nombre;
+	
+	public Ciudad(String nom) {
+		nombre = nom;
+	}
+
+}
---
+++ Prueba.java
@@ -0,0 +1,11 @@
+package Parte1;
+
+public class Prueba {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Ciudad c = new Ciudad("Huanguelen");
+	}
+
+}
